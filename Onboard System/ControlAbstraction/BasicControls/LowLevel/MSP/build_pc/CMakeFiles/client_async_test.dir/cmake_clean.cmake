7e1ba23e1c79e1becc57bf75c0dc665f854e879a
ashishkumar4 <ashishkmr472@gmail.com>
2018-12-18 00:35:05 +0530

    PC binaries added

---
+++ Onboard System/ControlAbstraction/BasicControls/LowLevel/MSP/build_pc/CMakeFiles/client_async_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_async_test.dir/examples/client_async_test.cpp.o"
+  "client_async_test.pdb"
+  "client_async_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client_async_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
