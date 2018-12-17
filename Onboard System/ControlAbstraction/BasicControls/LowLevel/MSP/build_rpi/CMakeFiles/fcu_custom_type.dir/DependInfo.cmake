dfcea69ae0cd779107fa68ec556faf90e4fd2872
root <ashishkmr472@gmail.com>
2018-12-17 08:26:23 +0000

    Build for RPI zero

---
+++ Onboard System/ControlAbstraction/BasicControls/LowLevel/MSP/build_rpi/CMakeFiles/fcu_custom_type.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Gardien-System/Onboard System/ControlAbstraction/BasicControls/LowLevel/MSP/examples/fcu_custom_type.cpp" "/root/Gardien-System/Onboard System/ControlAbstraction/BasicControls/LowLevel/MSP/build_rpi/CMakeFiles/fcu_custom_type.dir/examples/fcu_custom_type.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASIO_STANDALONE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc/msp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/Gardien-System/Onboard System/ControlAbstraction/BasicControls/LowLevel/MSP/build_rpi/CMakeFiles/msp_fcu.dir/DependInfo.cmake"
+  "/root/Gardien-System/Onboard System/ControlAbstraction/BasicControls/LowLevel/MSP/build_rpi/CMakeFiles/mspclient.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
