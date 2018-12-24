64f08aabe6c70c5e9462cce42ad63249357eb55a
ashishkumar4 <ashishkmr472@gmail.com>
2018-12-24 18:09:41 +0530

    New Failsafe for sockets

--- Onboard System/ControlAbstraction/BasicControls/source/ControlServer.cpp
+++ Onboard System/ControlAbstraction/BasicControls/source/ControlServer.cpp
@@ -11,6 +11,7 @@
 #include <iterator>
 #include <sstream>
 #include <thread> // std::thread
+#include <mutex>
 
 #include "BasicControls.h"
 
@@ -54,12 +55,13 @@ typedef void (*func_t)(int); //void function pointer
 func_t CHANNEL_HANDLER_TABLES[] = {&setThrottle, &setPitch, &setRoll, &setYaw, &setAux1, &setAux2};
 string CHANNEL_NAME_TABLES[] = {"throttle", "pitch", "roll", "yaw", "aux1", "aux2"};
 
+std::mutex smtx;
+
 class ControlServer
 {
-    static vector<int> server_fd;
-    static vector<int> socket_num;
+    static int* server_fd;
     vector<thread *> ListenerThreads;
-    static vector<struct sockaddr_in *> addresses;
+    static struct sockaddr_in** addresses;
 
     static int PORT_BASE;
 
@@ -90,12 +92,12 @@ class ControlServer
     {
         PORT_BASE = portBase;
         // We would first create several sockets and store their fds
-        CreateChannel(portBase + 0, 0); //  Throttle is         0
+        /*CreateChannel(portBase + 0, 0); //  Throttle is         0
         CreateChannel(portBase + 1, 1); //  Pitch is            1
         CreateChannel(portBase + 2, 2); //  Roll is             2
         CreateChannel(portBase + 3, 3); //  Yaw is              3
         CreateChannel(portBase + 4, 4); //  Aux1 is             4
-        CreateChannel(portBase + 5, 5); //  Aux2 is             5
+        CreateChannel(portBase + 5, 5); //  Aux2 is             5//*/
         // We Create six channels, each corresponding to six basic controls of drone
 
         // We would then create serveral threads, which would in turn listen to sockets
@@ -110,15 +112,18 @@ class ControlServer
     static void ChannelListeners(int i);
 };
 
-vector<int> ControlServer::server_fd;
-vector<int> ControlServer::socket_num;
-vector<struct sockaddr_in *> ControlServer::addresses;
+int* ControlServer::server_fd = new int[8];
+struct sockaddr_in** ControlServer::addresses = (struct sockaddr_in**)malloc(sizeof(struct sockaddr_in*)*8);
 
 int ControlServer::PORT_BASE;
 
 void ControlServer::ChannelListeners(int i)
 {
     int PORT = i + PORT_BASE;
+    CreateChannel(PORT, i);
+
+back:
+    smtx.lock();
     int sfd = server_fd[i];
     struct sockaddr_in *address = addresses[i];
     int new_socket;
@@ -132,13 +137,16 @@ void ControlServer::ChannelListeners(int i)
         exit(EXIT_FAILURE);
     }
     std::cout << "\nwaiting for incoming Connections...";
+    smtx.unlock();
     while (1)
     {
         int addrlen = sizeof(struct sockaddr);
         if ((new_socket = accept(sfd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
-            perror("accept");
-            exit(EXIT_FAILURE);
+            //perror("accept");
+            //exit(EXIT_FAILURE);
+            printf("accept");
+            goto back;
         }
         std::cout << "\nGot an incoming request...\n";
 
@@ -196,7 +204,7 @@ void ControlServer::ChannelListeners(int i)
 #endif
                 }
 #elif defined(STREAM_PROTOCOL_2)
-                for(int j = 0; j < valread; j++)
+                for (int j = 0; j < valread; j++)
                 {
                     CHANNEL_HANDLER_TABLES[i](int(buff[j]));
                 }
@@ -217,7 +225,14 @@ void ControlServer::ChannelListeners(int i)
 
 int ControlServer::CreateChannel(int port, int channel) // This would create a port for a particular channel
 {
+    smtx.lock();
     int sfd;
+    if (server_fd[channel])
+    {
+        //delete server_fd[channel];
+        //free((void*)server_fd[channel]);
+        //delete addresses[channel];
+    }
     struct sockaddr_in *address = new struct sockaddr_in;
     // Creating socket file descriptor
     if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -245,17 +260,10 @@ int ControlServer::CreateChannel(int port, int channel) // This would create a p
     }
     try
     {
-        if (server_fd.size() <= channel)
-        {
-            server_fd.push_back(sfd);
-            addresses.push_back(address);
-        }
-        else
-        {
-            // TODO: Free up the existing sockets
-            server_fd[channel] = sfd;
-            addresses[channel] = address;
-        }
+        // TODO: Free up the existing sockets
+        server_fd[channel] = sfd;
+        //delete addresses[channel];
+        addresses[channel] = address;
     }
     catch (...)
     {
@@ -263,6 +271,7 @@ int ControlServer::CreateChannel(int port, int channel) // This would create a p
         exit(EXIT_FAILURE);
     }
     cout << "Socket for port " << port << " Created Successfully!" << endl;
+    smtx.unlock();
     return 0;
 }
 int main(int argc, char const *argv[])
